9df25fae4ac9d0d67bda1c4cf2b6b8a477d8a15b
AlonBiner <alon.biner@gmail.com>
2024-09-30 16:47:46 +0300

    limited all items in trainset and test set to items with profiles

--- recommendation_of_disease_treatment_by_drug.py
+++ recommendation_of_disease_treatment_by_drug.py
@@ -4,9 +4,10 @@ from pandas import DataFrame, Series
 import re
 from recommendation_system import RecommendationSystem
 from analyze_profiles import calculate_drug_dist, calculate_disease_dist
+from analyze_profiles import load_medical_drugs_profiles
 
 status_to_rating_map = {
-    'Approved': 1, 'Unrated': 0,
+    'Approved': 1,
     'Withdrawn (Phase 1)': -1/2, 'Withdrawn (Phase 2)': -2/6, 'Withdrawn (Phase 3)': -1/6,
     'Suspended (Phase 1)': 1/2, 'Suspended (Phase 2)': 4/6, 'Suspended (Phase 3)': 5/6,
     'Terminated (Phase 1)': -1, 'Terminated (Phase 3)': -5/6, 'Terminated (Phase 2)': -4/6,
@@ -95,12 +96,12 @@ def _compute_drug_to_disease_matrix(utility_matrix: DataFrame):
     return utility_matrix.applymap(lambda rating: rating_to_status_map[rating] if pd.notna(rating) else rating)
     #return data.applymap(lambda status: status_to_rating_map[status])
 
-def postprocess(train_set: DataFrame, predictions: DataFrame):
+def postprocess(predictions: DataFrame):
     predictions['Rating'] = predictions['Rating'].apply(lambda rating: _adjust_to_rating_of_status(rating)) #v
-    unified_train_set_and_predictions = pd.concat([train_set, predictions], ignore_index=True)
-    updated_utility_matrix = unified_train_set_and_predictions.pivot(index='User', columns='Item', values='Rating')
-    updated_data = _compute_drug_to_disease_matrix(updated_utility_matrix)
-    return updated_data
+    # unified_train_set_and_predictions = pd.concat([train_set, predictions], ignore_index=True)
+    # updated_utility_matrix = unified_train_set_and_predictions.pivot(index='User', columns='Item', values='Rating')
+    # updated_data = _compute_drug_to_disease_matrix(updated_utility_matrix)
+    # return updated_data
 
 def _adjust_to_rating_of_status(rating):
     rating_of_statuses = np.array(list(status_to_rating_map.values()))
@@ -108,27 +109,36 @@ def _adjust_to_rating_of_status(rating):
     closest_index = abs_distances.argmin() # הדירוג שממזער את המטריצה
     return rating_of_statuses[closest_index]
 
-def load_profiles_of_drugs():
-    #TODO: Move load_items to analyze_profiles.py
-    profiles_of_drugs = pd.read_csv('drugbank_info_for_df.csv')
-    return profiles_of_drugs
 
 def load_profiles_of_diseases():
     profiles_of_diseases = pd.read_csv('nih_disease_info.csv')
     return profiles_of_diseases
 
-if __name__ == "__main__":
-    data = load_data('repodb_for_final_visualization_medical_profile.csv') #V
-    train_set, test_set = preprocess(data) #V??
-    recommendation_system = RecommendationSystem() # V
-    recommendation_system.fit() #  אין אימון כרגע... לעדכן.
-    #item_profiles = load_profiles_of_drugs() #TODO: עבור מוצר עבור פרופיל בדוק שעבור תרופה יש פרופיל שמתאים לה. . ... ...
-    #distance_func = calculate_drug_dist #V #TODO: אטען פרופילים של מוצרים אחרי שאלומה תתקן את הפונ' calculate_drug_dist
-    user_profiles = load_profiles_of_diseases()
-    distance_func = calculate_disease_dist
+def limit_to_items_with_profile(train_set, test_set, item_encoding, item_profiles):
+    limited_train_set = train_set[train_set['Item'].isin(item_profiles['DrugBank Accession Number'])]
+    limited_test_set = test_set[test_set['Item'].isin(item_profiles['DrugBank Accession Number'])]
+    return limited_train_set, limited_test_set
 
-    predictions = recommendation_system.predict(train_set, test_set, user_profiles, distance_func)
-    updated_data = postprocess(train_set, predictions)
+if __name__ == "__main__":
+    data = load_data('repodb_for_final_visualization_medical_profile.csv')
+    train_set, test_set = preprocess(data)
+    recommendation_system = RecommendationSystem()
+    recommendation_system.fit() #  אין אימון כרגע... לעדכן.
+
+    #user_profiles = load_profiles_of_diseases()
+    #distance_func_for_users = calculate_disease_dist
+
+    #predictions = recommendation_system.predict(train_set, test_set, user_profiles, distance_func_for_users, k=5)
+    #postprocess(predictions)
+
+    item_profiles = load_medical_drugs_profiles()
+    distance_func_for_items = calculate_drug_dist
+    drug_name_to_dbank_number_map = item_profiles.set_index('Generic Name')['DrugBank Accession Number'].to_dict()  # TODO: זה משהו שאלומה צריכה להגדיר אצלה. אני לא מור לדעת מה זה genric name.
+    # TODO: אפשר שתקודד אצלה בתור drug name
+    # TODO: לא אמור לדעת מה זה DrugBankAccessor Number אפשר שתקודד אצלה בתור drug band number...
+    train_set, test_set = limit_to_items_with_profile(train_set, test_set, 'DrugBank Accession Number', item_profiles)
+    predictions_item_item = recommendation_system.predict_item_item(train_set, test_set, item_profiles, distance_func_for_items, drug_name_to_dbank_number_map, k=5)
+    updated_data = postprocess(predictions_item_item)
     updated_data.to_csv('updated_data.csv')
 
     # איך נציג את התוצאות?
